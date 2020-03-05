50dce7235ce3c2d9eabcd5309784438aa341a3c4
Hauglid, Austin C <ahauglid@ou.edu>
2020-03-05 17:21:01 -0600

    done

---
+++ Homework4/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private bool mediaPlayerIsPlaying = false;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string url = "http://pcbstuou.w27.wh-2.com/webservices/3033/api/random/video";
+
+
+            string json = new WebClient().DownloadString(url);
+
+            Vid newVid = new Vid();
+
+            newVid = JsonConvert.DeserializeObject<Vid>(json);
+
+
+
+            var source = new Uri(newVid.url, UriKind.Absolute);
+            VidPlayer.Source = source;
+
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            if (mediaPlayerIsPlaying)
+            {
+                btnplay.Content = "Play";
+                VidPlayer.Pause();
+                mediaPlayerIsPlaying = false;
+            }
+            else
+            {
+
+                btnplay.Content = "Pause";
+                VidPlayer.Play();
+                mediaPlayerIsPlaying = true;
+            }
+
+
+
+        }
+
+        private void Btnstop_Click(object sender, RoutedEventArgs e)
+        {
+            VidPlayer.Stop();
+            mediaPlayerIsPlaying = false;
+            btnplay.Content = "Play";
+        }
+    }
+}
---
+++ Homework4/WpfApp1/Vid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Vid
+    {
+        public string description { get; set; }
+        public string url { get; set; }
+        public string id { get; set; }
+
+
+
+
+    }
+}
